210ae0df3781b078c88ab4ef5dd68b023eacb901
boris <borisdzotov@gmail.com>
2022-12-16 11:10:13 +0100

    Reset na proektot

--- src/main/java/mk/ukim/finki/wp/lab/model/exceptions/CourseNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.wp.lab.model.exceptions;
-
-public class CourseNotFoundException extends RuntimeException{
-    public CourseNotFoundException(){
-        super("Course not found!");
-    }
-}
